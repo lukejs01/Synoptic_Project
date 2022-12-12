4142fe1f9f316a42ef29e98f4f0c825657b159b0
lukejs01 <luke.jones@ada.ac.uk>
2022-12-12 12:19:27 +0000

    Initial commit

---
+++ src/main/java/com/synopticproject/synopticproject/SynopticProjectApplication.java
@@ -0,0 +1,13 @@
+package com.synopticproject.synopticproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynopticProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynopticProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/synopticproject/synopticproject/SynopticProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synopticproject.synopticproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynopticProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
